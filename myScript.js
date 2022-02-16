0585e691204ca6494bd0ae85eaa15924a1aa2373
Alexis García <lexisgasa3007@gmail.com>
2022-02-16 15:20:53 +0900

    update

--- myScript.js
+++ myScript.js
@@ -1,5 +1,37 @@
+/* Variables */
+
 let computerPick;
 let computerPickValue;
+let userPickName;
+let userPickValue;
+
+
+
+/* Main function */
+
+function roundStart (playerSelection, computerSelection) {
+    userPickName = prompt("Choose your option; rock, paper or scissors:")
+    userPickName = userPickName.charAt(0).toUpperCase() + userPickName.slice(1).toLowerCase();
+    (userPickName === "Rock") ? userPickValue = "1" : "" ;
+    (userPickName === "Paper") ? userPickValue = "2" : "" ;
+    (userPickName === "Scissors") ? userPickValue = "3" : "" ;
+    
+    computerPlay();
+
+    if (userPickValue === 1 && computerPick === 3 || userPickValue === 2 && computerPick === 1 || userPickValue === 3 && computerPick === 2) {
+        return `You win! ${userPickName} beats ${computerPickValue}!`;
+    } else if (userPickValue === 1 && computerPick === 2 || userPickValue === 2 && computerPick === 3 || userPickValue === 3 && computerPick === 1) {
+        return `You Lose! ${userPickName} loses against ${computerPickValue}!`;
+    } else {
+        return "Draw"; 
+    }
+}
+
+
+
+
+
+/* Subfunctions */
 
 function computerPlay () {
     computerPick = Math.floor(Math.random()*3)+1;

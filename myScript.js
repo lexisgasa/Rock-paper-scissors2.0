26c65c5ff1e9cb3a95037cd353c069d1268b50de
Alexis García <lexisgasa3007@gmail.com>
2022-02-16 18:26:21 +0900

    updated

--- myScript.js
+++ myScript.js
@@ -4,14 +4,33 @@ let computerPickValue;
 let computerPickName;
 let userPickName;
 let userPickValue;
-let playerSelection = userPlay();
-let computerSelection = computerPlay();
 
 
+/* Subfunctions */
 
-/* Main function 
+function computerPlay () {
+    computerPickValue = Math.floor(Math.random()*3)+1;
+    (computerPickValue === 1 ) ? computerPickName = "Rock" : " " ;
+    (computerPickValue === 2 ) ? computerPickName = "Paper" : " " ;
+    (computerPickValue === 3 ) ? computerPickName = "Scissors" : " " ;
+}
 
-function playRound (playerSelection, computerSelection) {
+function userPlay() {
+    userPickName = prompt("Choose your option; rock, paper or scissors:");
+    userPickName = userPickName.charAt(0).toUpperCase() + userPickName.slice(1).toLowerCase();
+    (userPickName === "Rock") ? userPickValue = 1 : "" ;
+    (userPickName === "Paper") ? userPickValue = 2 : "" ;
+    (userPickName === "Scissors") ? userPickValue = 3 : "" ;
+}
+
+
+/* Main function */
+
+function playRound () {
+    computerPlay();
+    userPlay();
+    console.log(computerPickValue);
+    console.log(userPickValue);
 
     if (userPickValue === 1 && computerPickValue === 3 || userPickValue === 2 && computerPickValue === 1 || userPickValue === 3 && computerPickValue === 2) {
         return `You win! ${userPickName} beats ${computerPickName}!`;
@@ -21,33 +40,6 @@ function playRound (playerSelection, computerSelection) {
         return "Draw"; 
     }
 }
-*/
-
-
-
-
-
-/* Subfunctions */
-
-function computerPlay () {
-    computerPickValue = Math.floor(Math.random()*3)+1;
-    (computerPickValue === 1 ) ? computerPickName= "Rock" : " " ;
-    (computerPickValue === 2 ) ? computerPickName = "Paper" : " " ;
-    (computerPickValue === 3 ) ? computerPickName = "Scissors" : " " ;
-    console.log(computerPickName);
-    console.log(computerPickValue);
-}
-
-function userPlay() {
-userPickName = prompt("Choose your option; rock, paper or scissors:");
-userPickName = userPickName.charAt(0).toUpperCase() + userPickName.slice(1).toLowerCase();
-(userPickName === "Rock") ? userPickValue = "1" : "" ;
-(userPickName === "Paper") ? userPickValue = "2" : "" ;
-(userPickName === "Scissors") ? userPickValue = "3" : "" ;
-console.log(userPickName);
-console.log(userPickValue);
-}
-
 /* values
 1 - ROCK
 2 - PAPER
